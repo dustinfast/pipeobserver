9ba2b70846f68239016eb8ae9f04f66e1d020fb3
Dustin Fast <dustin.fast@hotmail.com>
2018-09-12 12:52:39 -0800

    Added misc tests -DF

--- dotest.sh
+++ dotest.sh
@@ -6,10 +6,12 @@
 # gcc -O3 -o pipeobserver pipeobserver.c; ./pipeobserver allfiles [ ls -lat ] [ [ wc -l ];
 # gcc -O3 -o pipeobserver pipeobserver.c; ./pipeobserver allfiles [ ls -lat ] ] [ wc -l ]];
 # gcc -O3 -o pipeobserver pipeobserver.c; ./pipeobserver allfiles [ ls -lat ] ] [ wc -l ] ];
+# gcc -O3 -o pipeobserver pipeobserver.c; ./pipeobserver allfiles [ echo Unmatched bracket: ] ] [ wc -w ];
 #
 # Should Pass:
 # gcc -O3 -o pipeobserver pipeobserver.c; ./pipeobserver allfiles [ ps aux ] [ grep SCREEN ];
 # gcc -O3 -o pipeobserver pipeobserver.c; ./pipeobserver allfiles [ ls -lat ] [ wc -l ];
 # gcc -O3 -o pipeobserver pipeobserver.c; ./pipeobserver allfiles [ echo Hello world ] [ wc -w ];
 # gcc -O3 -o pipeobserver pipeobserver.c; ./pipeobserver allfiles [ echo Hello world ] [ wc -w ];
-gcc -O3 -o pipeobserver pipeobserver.c; ./pipeobserver allfiles [ cat pipeobserver.c pipeobserver.c ] [ wc -l ]
+# gcc -O3 -o pipeobserver pipeobserver.c; ./pipeobserver allfiles [ cat pipeobserver.c pipeobserver.c ] [ wc -l ]
+gcc -O3 -o pipeobserver pipeobserver.c; ./pipeobserver allfiles [ echo These are matched brackets: [ ] ] [ wc -w ];
